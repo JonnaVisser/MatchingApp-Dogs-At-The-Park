7164a61b6eea77b1e125693df84f9827d66aa8df
JonnaVisser <94176438+JonnaVisser@users.noreply.github.com>
2022-02-27 13:57:34 +0100

    routes

--- server.js
+++ server.js
@@ -1,34 +1,122 @@
-const express = requirel ('express')
+/*************************************
+ * Getting Started
+ *************************************/
+const express = require('express')
 
-const app = express
+/************************************
+ * Constants and Variables
+ *************************************/
+const app = express();
+const port = 3000;
+const hondenmaatjes= [
+    {
+        "id": 1,
+        "slug": "thirza-bommel",
+        "name": "Thirza Bommel",
+        "years": ["23", "3"],
+        "info": "single postieve meid die van praten houdt",
+        "zoekt": "hondenmaatje die af en toe lekker over koetjes en kalfjes wil praten"
+    },
+    {
+        "id": 2,
+        "slug": "roel-thunder",
+        "name": "Roel Thunder",
+        "years": ["27", "2"],
+        "info": "Ik ben een hondenmaatje met veel energie net als mijn geweldige hond, dit moeten wij af en toe kwijt",
+        "zoekt": "hondenmaatje waar we mee kunnen ravotten"
+    },
+    {
+        "id": 3,
+        "slug": "maria-asha",
+        "name": "Maria Asha",
+        "years": ["45", "10"],
+        "info": "huisvrouw met twee kinderen die naar school gaan",
+        "zoekt": "ouder hondenmaatje om tusssen de middag mee te kletsen"
+    },
+    {
+        "id": 4,
+        "slug": "joke-kaya",
+        "name": "Joke kaya",
+        "years": ["50", "4"],
+        "info": "ik ben sportief en hou van wandelen",
+        "zoekt": "hondenmaatje om een flink eind mee te wandelen en kletsen"
+    },
+    {
+        "id": 5,
+        "slug": "lisa-chick",
+        "name": "Lisa Chick",
+        "years": ["30", "8"],
+        "info": "Ik ben een echt meisje-meisje, en mijn kleine chick ook",
+        "zoekt": "hondenmaatje met klein schattig hondje die de perfecte match is voor chick en mij"
+    }
+]
 
+/************************************
+ * Routes
+ *************************************/
+app.get('/', (req, res) => {
+    let doc = '<!doctype html>';
+    doc += '<title>Hondenmaatjes</title>'
+    doc += '<h1>Hondenmaatjes</h1>'
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.get('/',(req, res) => {
-    res.send('hello world')
+    hondenmaatjes.forEach(hondenmaatje => {
+        doc += "<section>";
+        doc += `<h2>${hondenmaatje.name}</h2>`;
+        doc += "<h3>years:</h3>";
+        doc += `<h3>${hondenmaatje.info}</h3>`;
+        doc += "<ul>";
+        hondenmaatjes.years.forEach(years =>{
+            doc += `<li>${years}</li>`;
+        });
+        doc += "</ul>";
+        doc += `<a href="/hondenmaatjes/${hondenmaatjes.id}/${hondenmaatjes.slug}">more info</a>`
+        doc += "</section>";
+    });
+  res.send(doc)
 })
+
+/*variabele
+app.get('/name/:name', (req, res) => {
+    res.send(`hello ${req.params.name}`)
+  })*/
+
+
+
+
+
 
 
 app.listen(port, () => {
-console.log ('web server')
+  console.log(`Example app listening on port ${port}`)
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/***********************************
+ * Start Webserver
+ ************************************/
+ app.listen(port, () => {
+    console.log (`web server  running on http://localhost:${port}`)
+    })
